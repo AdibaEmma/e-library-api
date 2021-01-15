e08466a01d3cc12dfa67ed0c6955c91d700337fa
Emmanuel Adiba <eabaagah@gmail.com>
2021-01-15 21:52:06 +0000

    create student model

--- server/middlewares/auth.js
+++ server/middlewares/auth.js
@@ -7,19 +7,23 @@ exports.verifyToken = (req, res, next) => {
     const bearerHeader = req.headers["authorization"];
     const token = bearerHeader && bearerHeader.split(' ')[1]
 
-    if (token == null) {
-        return res.sendStatus(403)
-    } else {
-        jwt.verify(token, process.env.JWT_KEY, (err, user) => {
-            if (err) return res.sendStatus(401)
+    if (token == null) return res.sendStatus(403)
 
-            req.user = user;
+    jwt.verify(token, process.env.JWT_KEY, (err, authData) => {
+        if (err) {
+            return res.sendStatus(401)
+        } else {
+            res.status(200).json({
+                res: "authenticated"
+            })
 
-        })
+            req.user = authData;
+        }
+        
 
-        next()
-    }
+    })
 
+    next()
 
 
 }
---
+++ server/models/Student.js
@@ -0,0 +1,27 @@
+const mongoose = require("mongoose");
+
+const studentSchema = new mongoose.Schema({
+    _id: mongoose.Types._ObjectId,
+    name: {
+        type: [String,  "name must be string" ],
+        required: true,
+    },
+    email: {
+        type: String,
+        unique: true, 
+        required: [true, "email is required"],
+        lowercase: [true, "email must be in lowercase"]
+    },
+    indexNo: String,
+    program: String,
+    level: String,
+    password: {
+        type: String, 
+        minlength: [6, "password must be atleast six characters long"]
+    }
+    
+})
+
+const Student = mongoose.model("student", studentSchema);
+
+module.exports = Student;
